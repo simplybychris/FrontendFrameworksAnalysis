dbbd0acee2a220664eb7b040f157f449ce683a8f
simplybychris <59068947+simplybychris@users.noreply.github.com>
2022-11-12 15:03:58 +0100

    Added angular base components

--- apps/angular-app/src/app/app-routing.module.ts
+++ apps/angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SecondComponent} from "./second/second.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'second', component: SecondComponent
+  },
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
